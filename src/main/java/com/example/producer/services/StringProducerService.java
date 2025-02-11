8e1544930e856cc5e425b2f59b254ec97cf14072
hallyson <aparecidohallyson@gmail.com>
2025-02-11 12:29:22 -0300

    callback function for debug

--- src/main/java/com/example/producer/services/StringProducerService.java
+++ src/main/java/com/example/producer/services/StringProducerService.java
@@ -1,4 +1,5 @@
 package com.example.producer.services;
+
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,18 @@ public class StringProducerService {
         this.kafkaTemplate = kafkaTemplate;
     }
 
-    public void sendMessage(String text){
-        kafkaTemplate.send("str-topic", text);
+    public void sendMessage(String text) {
+        kafkaTemplate.send("str-topic", text).whenComplete(
+                (success, error) -> {
+                    if (error == null) {
+                        System.out.println("Mensagem enviada com sucesso para o tópico: " +
+                                success.getProducerRecord().topic() +
+                                ", Partição: " + success.getProducerRecord().partition() +
+                                ", Offset: " + success.getRecordMetadata().offset());
+                    } else {
+                        System.err.println("Erro ao enviar a mensagem: " + error.getMessage() + ", " + error);
+                    }
+                }
+        );
     }
-
 }
